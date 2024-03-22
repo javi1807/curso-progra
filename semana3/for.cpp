7c10c49d839ec295ba9a9e2ba29307688571986f
javi1807 <154531957+javi1807@users.noreply.github.com>
2024-03-22 12:06:03 -0500

    Update for.cpp

---
+++ semana3/for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    // Imprimir los números del 1 al 5
+    for (int i = 1; i <= 5; ++i) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
