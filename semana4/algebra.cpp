d5acc1d834bfb9831e7fc8645a3017f3dc8e451a
javi1807 <154531957+javi1807@users.noreply.github.com>
2024-03-22 11:53:35 -0500

    Update algebra.cpp

---
+++ semana4/algebra.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool a = true;
+    bool b = false;
+
+    // Expresión booleana con operador AND
+    bool resultado = a && b;
+    
+    cout << "a && b = " << resultado << endl; // Resultado: 0 (false)
+    
+    return 0;
+}
