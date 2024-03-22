4a3b015e5ec7d34193706c5cb98a7c4fb05efcaf
javi1807 <154531957+javi1807@users.noreply.github.com>
2024-03-22 12:06:31 -0500

    Update while.cpp

---
+++ semana3/while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Imprimir los números del 1 al 5
+    int i = 1;
+    while (i <= 5) {
+        cout << i << " ";
+        ++i;
+    }
+    cout << endl;
+    return 0;
+}
